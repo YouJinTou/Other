be82bd52b86a8d8b3263a1b25225deb93acf97fc
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2017-10-17 15:11:22 +0300

    Added exploits for unlimited health, no recoil, and rapid fire.

--- JTMN/JTMN/AC.Injectable/Listener.cpp
+++ JTMN/JTMN/AC.Injectable/Listener.cpp
@@ -2,6 +2,11 @@
 
 #include "Listener.h"
 
+Listener::Listener() :
+	healthAddress(GetAddressFromOffsets(HealthBaseAddress, HealthOffsets, HealthOffsetsCount))
+{
+}
+
 void Listener::Listen()
 {
 	while (true)
@@ -18,6 +23,26 @@ void Listener::HandleUserCommand()
 	{
 		HandleAmmoCommand();
 	}
+
+	if (GetAsyncKeyState(VK_F2))
+	{
+		HandleRapidFireCommand();
+	}
+
+	if (GetAsyncKeyState(VK_F3))
+	{
+		HandleRecoilCommand();
+	}
+
+	if (GetAsyncKeyState(VK_F4) || infiniteHealthOn)
+	{
+		if (GetAsyncKeyState(VK_F4))
+		{
+			infiniteHealthOn = !infiniteHealthOn;
+		}
+
+		HandleHealthCommand();
+	}
 }
 
 void Listener::HandleAmmoCommand()
@@ -26,11 +51,59 @@ void Listener::HandleAmmoCommand()
 
 	if (infiniteAmmoOn)
 	{
-		WriteToMemory(AmmoBaseAddress, AmmoTargetOpCode, AmmoTargetOpCodeSize);
+		WriteToMemory(AmmoBaseAddress, AmmoTargetOpCode, DefaultOpCodeSize);
 	}
 	else
 	{
-		WriteToMemory(AmmoBaseAddress, AmmoDefOpCode, AmmoDefOpCodeSize);
+		WriteToMemory(AmmoBaseAddress, AmmoDefOpCode, DefaultOpCodeSize);
+	}
+}
+
+void Listener::HandleRapidFireCommand()
+{
+	rapidFireOn = !rapidFireOn;
+
+	if (rapidFireOn)
+	{
+		WriteToMemory(RapidFireBaseAddress, RapidFireTargetOpCode, DefaultOpCodeSize);
+		WriteToMemory(AutomaticBaseAddress, AutomaticTargetOpCode, DefaultOpCodeSize);
+	}
+	else
+	{
+		WriteToMemory(RapidFireBaseAddress, RapidFireDefOpCode, DefaultOpCodeSize);
+		WriteToMemory(AutomaticBaseAddress, AutomaticDefOpCode, DefaultOpCodeSize);
+	}
+}
+
+void Listener::HandleRecoilCommand()
+{
+	antiRecoilOn = !antiRecoilOn;
+
+	if (antiRecoilOn)
+	{
+		for (size_t i = 0; i < RecoilBaseAddressesCount; i++)
+		{
+			WriteToMemory(RecoilBaseAddresses[i], RecoilTargetOpCodes[i], RecoilTargetOpCodeSizes[i]);
+		}
+	}
+	else
+	{
+		for (size_t i = 0; i < RecoilBaseAddressesCount; i++)
+		{
+			WriteToMemory(RecoilBaseAddresses[i], RecoilDefOpCodes[i], RecoilDefOpCodeSizes[i]);
+		}
+	}
+}
+
+void Listener::HandleHealthCommand()
+{
+	if (infiniteHealthOn)
+	{
+		WriteToMemory(healthAddress, TargetHealth, DefaultOpCodeSize);
+	}
+	else
+	{
+		WriteToMemory(healthAddress, DefaultHealth, DefaultOpCodeSize);
 	}
 }
 
@@ -46,4 +119,24 @@ void Listener::WriteToMemory(
 	memcpy((LPVOID)address, valueToWrite, pageByteSize);
 
 	VirtualProtect((LPVOID)address, pageByteSize, oldProtection, NULL);
+}
+
+DWORD Listener::GetAddressFromOffsets(
+	const DWORD baseAddress,
+	const DWORD offsets[],
+	const int offsetsCount) const
+{
+	if (offsetsCount == 0)
+	{
+		return baseAddress;
+	}
+
+	DWORD targetAddress = *(DWORD*)(baseAddress);
+
+	for (size_t i = 0; i < offsetsCount; i++)
+	{
+		targetAddress = DWORD(targetAddress + offsets[i]);
+	}
+
+	return targetAddress;
 }
--- JTMN/JTMN/AC.Injectable/Listener.h
+++ JTMN/JTMN/AC.Injectable/Listener.h
@@ -6,24 +6,58 @@
 class Listener
 {
 public:
+	Listener();
 	void Listen();
 private:
-	static constexpr unsigned long MaxTicks = 2000;
-	static constexpr DWORD AmmoBaseAddress = 0x4637E9;
-	static constexpr char* AmmoDefOpCode = "\xFF\x0E";
-	static constexpr int AmmoDefOpCodeSize = 2;
+	static constexpr int DefaultOpCodeSize = 2;
+
+	static constexpr DWORD AmmoBaseAddress = 0x004637E9;
 	static constexpr char* AmmoTargetOpCode = "\xFF\x06";
-	static constexpr int AmmoTargetOpCodeSize = 2;
+	static constexpr char* AmmoDefOpCode = "\xFF\x0E";
+
+	static constexpr DWORD RapidFireBaseAddress = 0x004637E4;
+	static constexpr char* RapidFireTargetOpCode = "\x90\x90";
+	static constexpr char* RapidFireDefOpCode = "\x89\x0A";
+
+	static constexpr DWORD AutomaticBaseAddress = 0x00463716;
+	static constexpr char* AutomaticTargetOpCode = "\x7D\x09";
+	static constexpr char* AutomaticDefOpCode = "\xEB\x09";
+
+	static constexpr int RecoilBaseAddressesCount = 7;
+	static constexpr DWORD RecoilBaseAddresses[RecoilBaseAddressesCount] =
+	{ 0x00463781, 0x00463783, 0x00463786, 0x00463787, 0x0046378B, 0x0046378C, 0x0046378E };
+	static constexpr char* RecoilTargetOpCodes[RecoilBaseAddressesCount] =
+	{ "\x90\x90", "\x90\x90\x90", "\x90", "\x90\x90\x90\x90", "\x90", "\x90\x90", "\x90\x90" };
+	static constexpr int RecoilTargetOpCodeSizes[] = { 2, 3, 1, 4, 1, 2, 2 };
+	static constexpr char* RecoilDefOpCodes[RecoilBaseAddressesCount] =
+	{ "\x8B\x16", "\x8B\x52\x14", "\x50", "\x8D\x4C\x24\x1C", "\x51", "\x8B\xCE", "\xFF\xD2" };
+	static constexpr int RecoilDefOpCodeSizes[RecoilBaseAddressesCount] = { 2, 3, 1, 4, 1, 2, 2 };
+
+	static constexpr DWORD HealthBaseAddress = 0x00509B74;
+	static constexpr DWORD HealthOffsets[] = { 0xF8 };
+	static constexpr int HealthOffsetsCount = 1;
+	static constexpr char* TargetHealth = "\x39\x5";
+	static constexpr char* DefaultHealth = "\x64\x0";
 private:
-	unsigned long elapsed = 0;
+	const DWORD healthAddress;
 	bool infiniteAmmoOn = false;
+	bool rapidFireOn = false;
+	bool antiRecoilOn = false;
+	bool infiniteHealthOn = false;
 private:
 	void HandleUserCommand();
 	void HandleAmmoCommand();
+	void HandleRapidFireCommand();
+	void HandleRecoilCommand();
+	void HandleHealthCommand();
 	void WriteToMemory(
-		const DWORD address, 
-		const char* valueToWrite, 
+		const DWORD address,
+		const char* valueToWrite,
 		const int pageByteSize);
+	DWORD GetAddressFromOffsets(
+		const DWORD baseAddress, 
+		const DWORD offsets[], 
+		const int offsetsCount) const;
 };
 
 #endif // !LISTENER
